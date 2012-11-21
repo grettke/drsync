b979dcc2136786a25001c383b7754f449eeb1fbb
Grant Rettke <grettke@acm.org>
2012-11-21 19:48:57 +0000

    Added a message to the install and remove script, a reminder to restart DrScheme

--- install-package.bat
+++ install-package.bat
@@ -1,1 +1,2 @@
 planet fileinject grettke drsync.plt 2 7
+echo You must restart DrScheme to use the tool
--- remove-package.bat
+++ remove-package.bat
@@ -1,1 +1,2 @@
 planet remove grettke drsync.plt 2 7
+echo You must restart DrScheme to unload the tool
